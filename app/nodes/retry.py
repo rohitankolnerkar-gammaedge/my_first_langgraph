fdbf38e483a2b19e7cffabc38981f517cfe376aa
rohitankolnerkar <rohit.ankolnerkar@gammaedge.io>
2026-02-25 19:06:49 +0530

    requirements addedd

--- app/nodes/retry.py
+++ app/nodes/retry.py
@@ -3,14 +3,13 @@ from app.graph.state import ReportState
 
 def retry(state: ReportState):
 
-    # If nothing to retry, do not update anything
+
     if not state.get("needs_retry"):
         return {}
 
     updated_retry_counts = dict(state.get("retry_counts", {}))
     updated_answers = list(state.get("answers", []))
 
-    # We will rebuild retry list safely
     remaining_retry_questions = []
 
     for failed_question in state["needs_retry"]:
